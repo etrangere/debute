8e31e6b9b41a34eb8347aa2c13610467603b5ec8
Urartu <urartu@MacBook-Pro-de-Urartu.local>
2017-11-16 23:10:33 +0100

    Edu_Project.Gallery Bootstrap initial implantation

--- 2.Edu_Project.Gallery/server.php
+++ 2.Edu_Project.Gallery/server.php
@@ -64,7 +64,7 @@ else
     echo"<br>File '$fileName' uploaded to server!<br>";
 }
 
-
+/*
 mysql_query($query) or die('Error,query failed');
 
 $result=mysql_query('SELECT * FROM images_table');
@@ -82,7 +82,7 @@ while ($row = mysql_fetch_assoc($result)) {
 
 
 mysql_close($link);
-
+*/
 ?>
 
 
@@ -100,13 +100,31 @@ mysql_close($link);
 
 </head>
 <body>
-<div class="container-fluide"  >
-  <center><h1>IMAGE GALLAERY</h1></center>
-
-
   
 
-        <?php
+
+     <div class="btn btn-info">
+        <ul><a href="login.php"> Login</a></ul>
+     </div>
+
+  
+   <div class="container">
+    
+     <div class="row">
+      
+       
+        <ul class="nav nav-pills" class="col-md-12">
+        <li><a href="" class="active">Home</a></li>
+        <li><a href="" >Upload your photos</a></li>
+        <li><a href="" >Help</a></li>
+        <li><a href="" >Contact us</a></li>
+        </ul>
+       
+      </div>
+  <div class="row">
+    <div class="col-md-3">
+      <div class="thumbnail">
+      <?php
            while(false != ($file = readdir($folder))) { 
 
             if ($file == ".." || $file == "." || $file == ".DS_Store") {
@@ -115,40 +133,110 @@ mysql_close($link);
  
            $file_path = $folder_path.$file;
            ?>
-           <div class="row">
-           <div clas= "col-md-12">
-           <form action="delete.php?id=<?php echo $file; ?>" method="post">
-            <a href="<?php echo $file_path;?>"><img src="<?php echo $file_path;?>" height="250" width="300" style="float:left;padding: 5px;"></a>
-          <input  type="submit" name="" value="Delete">
-            </form>
-           </div>
-           </div>
+                       
+                 <form action="delete.php?id=<?php echo $file; ?>" method="post">
+                  <a href="<?php echo $file_path;?>"><img src="<?php echo $file_path;?>"  height="250" width="250"></a>
+                   <input  class="btn btn-primary" type="submit" name="" value="Delete">
+                    </form>
+                   <button class="btn btn-primary" class="text-aligne-center">Like!</button> 
                   
-         <button type="button" class="btn btn-outline-info btn-sm">Like!</button> 
-          
-        <?php 
+              
+                
+               <?php 
 
-        }
+               }
   
-closedir($folder);
+               closedir($folder);
 
-?>
+       ?>
+       </div> 
+      </div>
+     </div>
+        <div>
+             <form action="" method = "post" enctype="multipart/form-data">
+             <input type = "file"  name="fileToUpload" value="Upload file"></br>
+             <input type = "submit" name="choose" value="Submit"></br>
+             </form>
+        </div>
+</div>
+<div class="container">
+  <h2>Image Gallery</h2>
+  <p>The .thumbnail class can be used to display an image gallery.</p>
+  <p>The .caption class adds proper padding and a dark grey color to text inside thumbnails.</p>
+  <p>Click on the images to enlarge them.</p>
+  <div class="row">
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="/w3images/lights.jpg" target="_blank">
+          <img src="/w3images/lights.jpg" alt="Lights" style="width:100%">
+          <div class="caption">
+            <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="/w3images/nature.jpg" target="_blank">
+          <img src="/w3images/nature.jpg" alt="Nature" style="width:100%">
+          <div class="caption">
+            <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="/w3images/fjords.jpg" target="_blank">
+          <img src="/w3images/fjords.jpg" alt="Fjords" style="width:100%">
+          <div class="caption">
+            <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
 
-<div >
-   <form action="" method = "post" enctype="multipart/form-data">
-   <input type = "file"  name="fileToUpload" value="Upload file"></br>
-   <input type = "submit" name="choose" value="Submit"></br>
-   </form>
+  <div class="row">
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="/w3images/lights.jpg" target="_blank">
+          <img src="/w3images/lights.jpg" alt="Lights" style="width:100%">
+          <div class="caption">
+            <button class="btn btn-primary">Like</button>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="/w3images/nature.jpg" target="_blank">
+          <img src="/w3images/nature.jpg" alt="Nature" style="width:100%">
+          <div class="caption">
+            <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="/w3images/fjords.jpg" target="_blank">
+          <img src="/w3images/fjords.jpg" alt="Fjords" style="width:100%">
+          <div class="caption">
+            <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
 </div>
 
-</div>
- 
 
-  <div class="footer">
-<hr>
-&copy; 2016 Все права защищены 
-
+<div class="footer">
+ <hr>
+ &copy; 2016 Все права защищены 
 </div>
+
 
 <script src="js/3.2.1/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
---
+++ 2.Edu_Project.Gallery/untitled.php
@@ -0,0 +1,31 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+  
+
+            <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
+            <script src="js/3.2.1/jquery.js"></script>
+            <script src="js/bootstrap.js"></script>
+</head>
+<body>
+<div class="navbar">     
+<ul class="nav nav-pills">
+  <li class="active"><a href="#">Home</a></li>
+  <li><a href="#">Menu 1</a></li>
+  <li><a href="#">Menu 2</a></li>
+  <li><a href="#">Menu 3</a></li>
+</ul>
+</div>
+<img src="/photo/013.jpg" class="rounded">
+<div class="tab-pane active">
+<li>Hello</li>
+
+</div>
+</body>
+</html>
+
+
