27ea5a69e7cfcef1e80d806fbbc7a8212e5c5361
etranger <etranger2008@yahoo.com>
2018-05-12 20:53:31 +0200

    10.Edu_symfony3_hotel no doctrine just controller and view level

---
+++ 10.Edu_Symfony3_hotel/var/cache/dev/twig/5b/5b3c195df90c243d9d551ee14c68264c38211331881faa4cbaa730f7d4627df0.php
@@ -0,0 +1,174 @@
+<?php
+
+/* :reservations:book.html.twig */
+class __TwigTemplate_d4930fd674f30f0101c3a61bd5ec0ae32c073b6ec456414d076e27d955b2e8bd extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        $this->parent = $this->loadTemplate("base.html.twig", ":reservations:book.html.twig", 1);
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", ":reservations:book.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", ":reservations:book.html.twig"));
+
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        // line 4
+        echo "
+    <div class=\"row\">
+      <div class=\"medium-12 large-12 columns\">
+        <h4>Clients/Booking</h4>
+        <div class=\"medium-2  columns\">BOOKING FOR: ";
+        // line 8
+        echo twig_escape_filter($this->env, (isset($context["id_client"]) ? $context["id_client"] : $this->getContext($context, "id_client")), "html", null, true);
+        echo "</div>
+        <div class=\"medium-2  columns\"><b>Client Name</b></div>
+        <form action=\"\" method=\"post\">
+          <div class=\"medium-1  columns\">FROM:</div>
+          <div class=\"medium-2  columns\"><input name=\"\" value=\"\" type=\"text\" class=\"datepicker\" /></div>
+          <div class=\"medium-1  columns\">TO:</div>
+          <div class=\"medium-2  columns\"><input name=\"\" value=\"\" type=\"text\" class=\"datepicker\" /></div>
+          <div class=\"medium-2  columns\"><input class=\"button\" type=\"submit\" value=\"SEARCH\" /></div>
+        </form>
+
+        <table class=\"stack\">
+          <thead>
+            <tr>
+              <th width=\"200\">Room</th>
+              <th width=\"200\">Availability</th>
+              <th width=\"200\">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+          
+            <tr>
+              <td>101</td>
+              <td>
+                <div class=\"callout success\">
+                    <h7>Available</h7>
+                </div>
+              </td>
+              <td>
+                <a class=\"hollow button warning\" href=\"#\">BOOK NOW</a>
+              </td>
+            </tr>
+          
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return ":reservations:book.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  55 => 8,  49 => 4,  40 => 3,  11 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("{% extends 'base.html.twig' %}
+
+{% block body %}
+
+    <div class=\"row\">
+      <div class=\"medium-12 large-12 columns\">
+        <h4>Clients/Booking</h4>
+        <div class=\"medium-2  columns\">BOOKING FOR: {{ id_client }}</div>
+        <div class=\"medium-2  columns\"><b>Client Name</b></div>
+        <form action=\"\" method=\"post\">
+          <div class=\"medium-1  columns\">FROM:</div>
+          <div class=\"medium-2  columns\"><input name=\"\" value=\"\" type=\"text\" class=\"datepicker\" /></div>
+          <div class=\"medium-1  columns\">TO:</div>
+          <div class=\"medium-2  columns\"><input name=\"\" value=\"\" type=\"text\" class=\"datepicker\" /></div>
+          <div class=\"medium-2  columns\"><input class=\"button\" type=\"submit\" value=\"SEARCH\" /></div>
+        </form>
+
+        <table class=\"stack\">
+          <thead>
+            <tr>
+              <th width=\"200\">Room</th>
+              <th width=\"200\">Availability</th>
+              <th width=\"200\">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+          
+            <tr>
+              <td>101</td>
+              <td>
+                <div class=\"callout success\">
+                    <h7>Available</h7>
+                </div>
+              </td>
+              <td>
+                <a class=\"hollow button warning\" href=\"#\">BOOK NOW</a>
+              </td>
+            </tr>
+          
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+{% endblock %}
+", ":reservations:book.html.twig", "/Applications/XAMPP/xamppfiles/htdocs/debute/10.Project_02_02/app/Resources/views/reservations/book.html.twig");
+    }
+}
---
+++ 10.Edu_Symfony3_hotel/var/cache/dev/twig/b1/b15c4086cb58424b86d056d80e48bbebc54f441ad96f34fa85636ab2d9efccd1.php
@@ -0,0 +1,186 @@
+<?php
+
+/* clients/index.html.twig */
+class __TwigTemplate_d68447baef814a55b6e302d262e1d53cea64b09acdad4a696fdfa4c0ef05cc30 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        $this->parent = $this->loadTemplate("base.html.twig", "clients/index.html.twig", 1);
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "clients/index.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "clients/index.html.twig"));
+
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        // line 4
+        echo "
+    <div class=\"row\">
+      <div class=\"medium-12 large-12 columns\">
+        <h4>Clients</h4>
+        <div class=\"medium-2  columns\"><a class=\"button\" href=\"";
+        // line 8
+        echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("new_client", array("id_client" => 1));
+        echo "\">ADD NEW CLIENT</a></div>
+
+        <table class=\"stack\">
+          <thead>
+            <tr>
+              <th width=\"200\">Name</th>
+              <th width=\"200\">Email</th>
+              <th width=\"200\">Action</th>
+            </tr>
+          </thead>
+
+
+                ";
+        // line 20
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["clients"]) ? $context["clients"] : $this->getContext($context, "clients")));
+        foreach ($context['_seq'] as $context["_key"] => $context["client"]) {
+            // line 21
+            echo "                <tr>
+                <td>";
+            // line 22
+            echo twig_escape_filter($this->env, twig_upper_filter($this->env, $this->getAttribute($context["client"], "title", array())), "html", null, true);
+            echo ". ";
+            echo twig_escape_filter($this->env, $this->getAttribute($context["client"], "name", array()), "html", null, true);
+            echo " ";
+            echo twig_escape_filter($this->env, $this->getAttribute($context["client"], "last_name", array()), "html", null, true);
+            echo "</td>
+                <td>";
+            // line 23
+            echo twig_escape_filter($this->env, $this->getAttribute($context["client"], "Email", array()), "html", null, true);
+            echo "</td>
+                <td>
+                    <a class=\"hollow button\" href=\"";
+            // line 25
+            echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("modify_client", array("id_client" => $this->getAttribute($context["client"], "id", array()))), "html", null, true);
+            echo "\">EDIT</a>
+                    <a class=\"hollow button warning\" href=\"";
+            // line 26
+            echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("booking", array("id_client" => $this->getAttribute($context["client"], "id", array()))), "html", null, true);
+            echo "\">BOOK A ROOM</a>
+                </td>
+              </tr>
+              ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['client'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 30
+        echo "              <tr>
+
+              </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "clients/index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  104 => 30,  94 => 26,  90 => 25,  85 => 23,  77 => 22,  74 => 21,  70 => 20,  55 => 8,  49 => 4,  40 => 3,  11 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("{% extends 'base.html.twig' %}
+
+{% block body %}
+
+    <div class=\"row\">
+      <div class=\"medium-12 large-12 columns\">
+        <h4>Clients</h4>
+        <div class=\"medium-2  columns\"><a class=\"button\" href=\"{{ path(\"new_client\",{'id_client' :1}) }}\">ADD NEW CLIENT</a></div>
+
+        <table class=\"stack\">
+          <thead>
+            <tr>
+              <th width=\"200\">Name</th>
+              <th width=\"200\">Email</th>
+              <th width=\"200\">Action</th>
+            </tr>
+          </thead>
+
+
+                {%  for client in clients %}
+                <tr>
+                <td>{{ client.title | upper }}. {{ client.name }} {{ client.last_name }}</td>
+                <td>{{ client.Email }}</td>
+                <td>
+                    <a class=\"hollow button\" href=\"{{ path(\"modify_client\", { 'id_client': client.id }) }}\">EDIT</a>
+                    <a class=\"hollow button warning\" href=\"{{ path(\"booking\", { 'id_client': client.id }) }}\">BOOK A ROOM</a>
+                </td>
+              </tr>
+              {%  endfor %}
+              <tr>
+
+              </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+{% endblock %}
+", "clients/index.html.twig", "/Applications/XAMPP/xamppfiles/htdocs/debute/10.Edu_Symfony3_hotel/app/Resources/views/clients/index.html.twig");
+    }
+}
