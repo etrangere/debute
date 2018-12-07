92dc0ba08cbb40413909cda8d6a3384fd25fbb31
root <etranger2008@yahoo.com>
2018-12-07 01:40:45 +0100

    Project 10 about and contact section refactoring,add some photos

--- 10.Edu_Symfony3_hotel/var/cache/dev/twig/d9/d9b85f6168e6e076fd11031765b913b945df358986d7767cb7c32589534cddda.php
+++ 10.Edu_Symfony3_hotel/var/cache/dev/twig/d9/d9b85f6168e6e076fd11031765b913b945df358986d7767cb7c32589534cddda.php
@@ -1,6 +1,6 @@
 <?php
 
-/* :home:contact.html.twig */
+/* home/contact.html.twig */
 class __TwigTemplate_906703fa4fb8748439853b1bdda3e7818cea1f1cfd5be3c9e9ca14fc85c04cc0 extends Twig_Template
 {
     private $source;
@@ -12,7 +12,7 @@ class __TwigTemplate_906703fa4fb8748439853b1bdda3e7818cea1f1cfd5be3c9e9ca14fc85c
         $this->source = $this->getSourceContext();
 
         // line 1
-        $this->parent = $this->loadTemplate("base.html.twig", ":home:contact.html.twig", 1);
+        $this->parent = $this->loadTemplate("base.html.twig", "home/contact.html.twig", 1);
         $this->blocks = array(
             'body' => array($this, 'block_body'),
         );
@@ -26,10 +26,10 @@ class __TwigTemplate_906703fa4fb8748439853b1bdda3e7818cea1f1cfd5be3c9e9ca14fc85c
     protected function doDisplay(array $context, array $blocks = array())
     {
         $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
-        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", ":home:contact.html.twig"));
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "home/contact.html.twig"));
 
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
-        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", ":home:contact.html.twig"));
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "home/contact.html.twig"));
 
         $this->parent->display($context, array_merge($this->blocks, $blocks));
         
@@ -50,11 +50,19 @@ class __TwigTemplate_906703fa4fb8748439853b1bdda3e7818cea1f1cfd5be3c9e9ca14fc85c
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
 
         // line 7
-        echo "    <div class=\"container\">
+        echo "
+<h3>Contact Information</h3>
+   <p align=\"center\">
+   <li>198 West 21th Street,Suite 721 New York NY 10016</li>
+   <li>Tel:+ 1235 1155 98</li>
+   <li>Email:info@yoursite.com</li></p>
+
+
+    <div class=\"container\">
         <div class=\"row align-items-center\">
             <div class=\"col-md-10\">
 
-                <h2 class=\"heading\">Get In Touch</h2>
+                <h4 class=\"heading\">Get In Touch</h4>
             </div>
         </div>
     </div>
@@ -81,7 +89,8 @@ class __TwigTemplate_906703fa4fb8748439853b1bdda3e7818cea1f1cfd5be3c9e9ca14fc85c
                             <textarea name=\"\" id=\"\" cols=\"30\" rows=\"7\" class=\"form-control px-3 py-3\" placeholder=\"Message\"></textarea>
                         </div>
                         <div class=\"form-group\">
-                            <input type=\"submit\" value=\"Send Message\" class=\"btn btn-primary py-3 px-5\">
+                           <!-- <input type=\"submit\" value=\"Send Message\" class=\"btn btn-primary py-3 px-5\">-->
+                            <a class=\"medium-6  columns\" href=\"#\"><input type=\"submit\" class=\"button\"  value=\"SEND MESSAGE\"></a>
                         </div>
                     </form>
 
@@ -90,6 +99,15 @@ class __TwigTemplate_906703fa4fb8748439853b1bdda3e7818cea1f1cfd5be3c9e9ca14fc85c
 
         </div>
     </div>
+
+        <h5>Opening Hours</h5>
+        <p align=\"justify\"><li>Monday: 08: - 22:00</li>
+        <li>Tuesday: 08: - 22:00</li>
+        <li>Wednesday: 08: - 22:00</li>
+        <li>Thursday: 08: - 22:00</li>
+        <li>Friday: 08: - 22:00</li>
+        <li>Saturday: 08: - 22:00</li>
+        <li>Sunday: 08: - 22:00</li></p>
 
 ";
         
@@ -102,7 +120,7 @@ class __TwigTemplate_906703fa4fb8748439853b1bdda3e7818cea1f1cfd5be3c9e9ca14fc85c
 
     public function getTemplateName()
     {
-        return ":home:contact.html.twig";
+        return "home/contact.html.twig";
     }
 
     public function isTraitable()
@@ -123,11 +141,19 @@ class __TwigTemplate_906703fa4fb8748439853b1bdda3e7818cea1f1cfd5be3c9e9ca14fc85c
 
 
 {% block body %}
+
+<h3>Contact Information</h3>
+   <p align=\"center\">
+   <li>198 West 21th Street,Suite 721 New York NY 10016</li>
+   <li>Tel:+ 1235 1155 98</li>
+   <li>Email:info@yoursite.com</li></p>
+
+
     <div class=\"container\">
         <div class=\"row align-items-center\">
             <div class=\"col-md-10\">
 
-                <h2 class=\"heading\">Get In Touch</h2>
+                <h4 class=\"heading\">Get In Touch</h4>
             </div>
         </div>
     </div>
@@ -154,7 +180,8 @@ class __TwigTemplate_906703fa4fb8748439853b1bdda3e7818cea1f1cfd5be3c9e9ca14fc85c
                             <textarea name=\"\" id=\"\" cols=\"30\" rows=\"7\" class=\"form-control px-3 py-3\" placeholder=\"Message\"></textarea>
                         </div>
                         <div class=\"form-group\">
-                            <input type=\"submit\" value=\"Send Message\" class=\"btn btn-primary py-3 px-5\">
+                           <!-- <input type=\"submit\" value=\"Send Message\" class=\"btn btn-primary py-3 px-5\">-->
+                            <a class=\"medium-6  columns\" href=\"#\"><input type=\"submit\" class=\"button\"  value=\"SEND MESSAGE\"></a>
                         </div>
                     </form>
 
@@ -164,7 +191,16 @@ class __TwigTemplate_906703fa4fb8748439853b1bdda3e7818cea1f1cfd5be3c9e9ca14fc85c
         </div>
     </div>
 
+        <h5>Opening Hours</h5>
+        <p align=\"justify\"><li>Monday: 08: - 22:00</li>
+        <li>Tuesday: 08: - 22:00</li>
+        <li>Wednesday: 08: - 22:00</li>
+        <li>Thursday: 08: - 22:00</li>
+        <li>Friday: 08: - 22:00</li>
+        <li>Saturday: 08: - 22:00</li>
+        <li>Sunday: 08: - 22:00</li></p>
+
 {% endblock %}
-", ":home:contact.html.twig", "/var/www/10.Edu_Symfony3_hotel/app/Resources/views/home/contact.html.twig");
+", "home/contact.html.twig", "/var/www/10.Edu_Symfony3_hotel/app/Resources/views/home/contact.html.twig");
     }
 }
