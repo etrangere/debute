0449164c69dcae502eb48143af44f2bb5267803c
root <you@example.com>
2019-01-04 18:05:00 +0100

    Project 10,mysql backup file update users

--- 10.Edu_Symfony3_hotel/db/10.Edu_Symfony3_hotel.sql
+++ 10.Edu_Symfony3_hotel/db/10.Edu_Symfony3_hotel.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Sam 29 Décembre 2018 à 00:57
+-- Généré le :  Ven 04 Janvier 2019 à 18:01
 -- Version du serveur :  5.7.24-0ubuntu0.16.04.1
--- Version de PHP :  7.2.13-1+ubuntu16.04.1+deb.sury.org+1
+-- Version de PHP :  7.1.24-1+ubuntu16.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -35,7 +35,7 @@ CREATE TABLE `client` (
   `zip_code` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   `city` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `state` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `client_email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `room_type` int(11) NOT NULL,
   `adult` int(11) NOT NULL,
   `child` int(11) DEFAULT NULL,
@@ -47,22 +47,8 @@ CREATE TABLE `client` (
 -- Contenu de la table `client`
 --
 
-INSERT INTO `client` (`id`, `title`, `name`, `last_name`, `address`, `zip_code`, `city`, `state`, `email`, `room_type`, `adult`, `child`, `baby`, `id_room`) VALUES
-(26, 'mr selected', 'SDFQSDFSDFG', 'QGSDFQSFDG', 'SQFDGS', '90', 'sqgSDG', 'SQDFGSDG', 'sdgSDG', 2, 1, 1, 1, '2'),
-(30, 'mr selected', 'zaeraezrzaer', 'zetzetazerazr', 'zttrz\'r', '89', 'zerzer', 'zezet', 'zzerzer', 1, 1, 1, 1, '3'),
-(31, 'mr', 'zertzert', 'eztrzetr', 'eztrzetr', '99', 'zertzertzert', 'erteztr', 'zerzer', 1, 1, 1, 1, '2'),
-(32, 'mr', 'ererererer', 'rtrtrtr', 'rtyerty', '66', 'sdfsdfgsdfg', 'dfghfdhdfhg', 'fdghfdghdfhg', 1, 1, 1, 1, '2'),
-(33, 'mr', 'sdsfd', 'erteztzert', 'zertazetzes', '888', 'dfgqegert', 'ertezrtzetr', 'ezrtz\'y\'(yzt', 1, 1, 0, 0, '1'),
-(34, 'mr', 'qsdqsdfsqdf', 'qdfgqdfgqf', 'qsdfgSDGQSDFG', '99', 'SDQFGQSDFG', 'ERYRTYERTY', 'TRUQE<EQGQ', 1, 1, 0, 0, '2'),
-(35, 'mr', 'Jdjdj', 'Hdjjnd', 'Hdhjss', '3737', 'Zhzhj', 'Udhdh', 'Hdhdh', 2, 2, 0, 0, '3'),
-(36, 'mr', 'vjfghj', 'ghjgj', 'gfhjg', '45', 'vnbn', 'vbnvbn', 'bcv', 1, 1, 1, 1, '2'),
-(37, 'mr', 'Ghgg', 'Gghvv', 'Gggg', '56', 'Ggjh', 'Hfgg', 'Gfdff', 2, 2, 1, 1, '3'),
-(38, 'mr', 'Ghgg', 'Ffgg', 'Fgg', '45', 'Vvgh', 'Vhh', 'Ffggg', 2, 2, 2, 0, '3'),
-(39, 'mr', 'sdssgsdf', 'dsfg', 'sdfgsdfg', '12', 'sdfgsdfg', 'sdfgsdfg', 'sdfgsdfgsdfg', 1, 1, 1, 0, '2'),
-(40, 'mr', 'sdfgsdfg', 'sdfgsdfg', 'sdfgsdfgsdg', '123', 'sdfgsdfg', 'sdfgsdfg', 'sdfgsdfg', 2, 2, 1, 0, '2'),
-(41, 'mr', 'Jdjdjd', 'Jdjdhd', 'Jxjxj', '66', 'Jzjzjx', 'Zjjsh', 'Yzhzhz', 2, 2, 2, 0, '3'),
-(42, 'mr', 'dfgsdfg', 'dsfgbh', 'ggdf', '22', 'gg', 'hhf', 'fghfgh', 2, 2, 0, 0, '1'),
-(43, 'mrs', 'Margarite', 'HHh', 'lmkjhlkjhl', '12', 'sdfsdf', 'sdf', 'sdfsdffs', 2, 2, 1, 0, '2');
+INSERT INTO `client` (`id`, `title`, `name`, `last_name`, `address`, `zip_code`, `city`, `state`, `client_email`, `room_type`, `adult`, `child`, `baby`, `id_room`) VALUES
+(64, 'mr', 'test', 'test hhhh', 'mlkjmljk', '333', 'sdfqsdfqs', 'ghjkhjk', 'g.khachatrian@free.fr', 1, 2, 1, 1, '1');
 
 -- --------------------------------------------------------
 
@@ -83,21 +69,7 @@ CREATE TABLE `reservation` (
 --
 
 INSERT INTO `reservation` (`id`, `client_id`, `room_id`, `date_in`, `date_out`) VALUES
-(12, 26, 2, '2018-12-26 00:00:00', '2018-12-28 00:00:00'),
-(16, 30, 3, '2018-12-25 00:00:00', '2018-12-27 00:00:00'),
-(17, 31, 2, '2018-12-25 20:18:00', '2018-12-27 20:18:00'),
-(18, 32, 2, '2018-12-23 20:18:00', '2018-12-26 20:18:00'),
-(19, 33, 1, '2018-12-24 20:18:00', '2018-12-28 20:18:00'),
-(20, 34, 2, '2018-12-25 20:18:00', '2018-12-24 20:18:00'),
-(21, 35, 3, '2018-01-23 20:19:00', '2018-12-22 20:18:00'),
-(22, 36, 2, '2018-12-25 20:18:00', '2018-12-25 20:18:00'),
-(23, 37, 3, '2018-01-04 20:19:00', '2018-01-05 20:19:00'),
-(24, 38, 3, '2018-12-27 20:18:00', '2018-01-05 20:19:00'),
-(25, 39, 2, '2018-12-31 20:18:00', '2018-01-04 20:19:00'),
-(26, 40, 2, '2018-12-28 20:18:00', '2018-12-29 20:18:00'),
-(27, 41, 3, '2018-01-05 20:19:00', '2018-01-05 20:19:00'),
-(28, 42, 1, '2018-12-31 20:18:00', '2018-01-04 20:19:00'),
-(29, 43, 2, '2018-12-28 20:18:00', '2018-12-29 20:18:00');
+(46, 64, 1, '2019-01-04 20:19:00', '2019-01-05 20:19:00');
 
 -- --------------------------------------------------------
 
@@ -123,6 +95,28 @@ INSERT INTO `room` (`id`, `name`, `floor`, `description`, `room_type`, `price`) 
 (2, '102', 1, 'Twin 2-adult/child/wifi', 2, 75),
 (3, '103', 1, 'Triple 3-adult/child/wifi', 3, 105);
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `roles` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `password`, `roles`) VALUES
+(2, 'gggg@mail.ru', '$2y$13$oLrFOqYN1zCL0UsXxNosHO9EUH8eEHxUXDeQB9k4jAggcngXvgwyi', 'a:0:{}'),
+(3, 'ggggg@mail.ru', '$2y$13$k7Dqu1lgTx7Jr.bfdDGQleNXQya7Go3LPRpGHnhgwyy39SVOiwzbS', 'a:0:{}'),
+(4, 'gg@mail.ru', '$2y$13$NIwICN.ZhJMD4288EJ4bbO8YY.FyIgzBEmw1N8gqH3KPgLmhEUvHS', 'ROLE_ADMIN');
+
 --
 -- Index pour les tables exportées
 --
@@ -132,7 +126,7 @@ INSERT INTO `room` (`id`, `name`, `floor`, `description`, `room_type`, `price`) 
 --
 ALTER TABLE `client`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `UNIQ_C7440455E7927C74` (`email`);
+  ADD UNIQUE KEY `UNIQ_C744045544FFE0C3` (`client_email`);
 
 --
 -- Index pour la table `reservation`
@@ -149,6 +143,12 @@ ALTER TABLE `room`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT pour les tables exportées
 --
 
@@ -156,17 +156,22 @@ ALTER TABLE `room`
 -- AUTO_INCREMENT pour la table `client`
 --
 ALTER TABLE `client`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 --
 -- AUTO_INCREMENT pour la table `reservation`
 --
 ALTER TABLE `reservation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 --
 -- AUTO_INCREMENT pour la table `room`
 --
 ALTER TABLE `room`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- Contraintes pour les tables exportées
 --
---
+++ 10.Edu_Symfony3_hotel/var/cache/dev/twig/b7/b7de19333895a3eb19494f3860fffcadb6d713853e2249748501fcd8ac6e9761.php
@@ -0,0 +1,363 @@
+<?php
+
+/* base.html.twig */
+class __TwigTemplate_377bfd8ad0cb9b07574753b54ab63c729d134a589b6ada3b1ee0e19d7c743e8d extends Twig_Template
+{
+    private $source;
+
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = array(
+            'title' => array($this, 'block_title'),
+            'stylesheets' => array($this, 'block_stylesheets'),
+            'top_bar' => array($this, 'block_top_bar'),
+            'body' => array($this, 'block_body'),
+            'javascripts' => array($this, 'block_javascripts'),
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "base.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "base.html.twig"));
+
+        // line 1
+        echo "<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=\"UTF-8\" />
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+        <title>";
+        // line 6
+        $this->displayBlock('title', $context, $blocks);
+        echo "</title>
+        ";
+        // line 7
+        $this->displayBlock('stylesheets', $context, $blocks);
+        // line 8
+        echo "        <link rel=\"icon\" type=\"image/x-icon\" href=\"";
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("favicon.ico"), "html", null, true);
+        echo "\" />
+        <link rel=\"stylesheet\" href=\"";
+        // line 9
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("css/foundation.css"), "html", null, true);
+        echo "\">
+        <link rel=\"stylesheet\" href=\"";
+        // line 10
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("css/app.css"), "html", null, true);
+        echo "\">
+        <link rel=\"stylesheet\" href=\"";
+        // line 11
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("css/sidenav.css"), "html", null, true);
+        echo "\">
+        <link rel=\"stylesheet\" href=\"";
+        // line 12
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("pickadate/lib/themes/default.css"), "html", null, true);
+        echo "\">
+        <link rel=\"stylesheet\" href=\"";
+        // line 13
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("pickadate/lib/themes/default.date.css"), "html", null, true);
+        echo "\">
+
+    </head>
+    <body>
+    ";
+        // line 17
+        $this->displayBlock('top_bar', $context, $blocks);
+        // line 35
+        echo "    <noscript><h4 align=\"center\">For full functionality of this page it is necessary to enable JavaScript.
+            Here are the <a href=\"http://www.enable-javascript.com\" target=\"_blank\">
+                instructions how to enable JavaScript in your web browser</a></h4></noscript>
+    <br>
+
+        ";
+        // line 40
+        $this->displayBlock('body', $context, $blocks);
+        // line 41
+        echo "    
+
+    <div class=\"row column\">
+      <hr>
+      <ul class=\"menu\">
+        <li class=\"float-right\">Copyright 2017</li>
+      </ul>
+    </div>
+
+    <script>
+      \$(document).foundation();
+    </script>
+
+        ";
+        // line 54
+        $this->displayBlock('javascripts', $context, $blocks);
+        // line 55
+        echo "        <script src=\"";
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("js/vendor/jquery.js"), "html", null, true);
+        echo "\"></script>
+        <script src=\"";
+        // line 56
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("js/vendor/what-input.js"), "html", null, true);
+        echo "\"></script>
+        <script src=\"";
+        // line 57
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("js/vendor/foundation.js"), "html", null, true);
+        echo "\"></script>
+        <script src=\"";
+        // line 58
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("js/app.js"), "html", null, true);
+        echo "\"></script>
+        <script src=\"";
+        // line 59
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("pickadate/lib/picker.js"), "html", null, true);
+        echo "\"></script>
+        <script src=\"";
+        // line 60
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("pickadate/lib/picker.date.js"), "html", null, true);
+        echo "\"></script>
+        <script src='https://www.google.com/recaptcha/api.js'></script>
+    <script>
+        \$('.datepicker').pickadate(
+            {
+                //format: 'yyyy-mm-dd',
+                //formatSubmit: 'yyyy-mm-dd',
+                format: 'd mmmm, yyyy',
+                min: new Date('today'),
+                max: new Date(2019,12,31),
+                today: 'Today'
+            }
+        );
+
+    </script>
+    </body>
+</html>
+";
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 6
+    public function block_title($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "title"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "title"));
+
+        echo "Hotel App";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 7
+    public function block_stylesheets($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "stylesheets"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "stylesheets"));
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 17
+    public function block_top_bar($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "top_bar"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "top_bar"));
+
+        // line 18
+        echo "        <!-- Start Top Bar -->
+    <div class=\"top-bar\">
+      <div class=\"row\">
+        <div class=\"top-bar-left\">
+          <ul class=\"dropdown menu\" data-dropdown-menu>
+            <li><a href=\"";
+        // line 23
+        echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("home");
+        echo "\">Home</a></li>
+              <li><a href=\"";
+        // line 24
+        echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("about_us");
+        echo "\">About us</a></li>
+              <li><a href=\"";
+        // line 25
+        echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("location");
+        echo "\">How to locate</a></li>
+              <li><a href=\"";
+        // line 26
+        echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("contact");
+        echo "\">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- End Top Bar -->
+
+
+    ";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 40
+    public function block_body($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        echo " ";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 54
+    public function block_javascripts($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "javascripts"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "javascripts"));
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "base.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  252 => 54,  234 => 40,  215 => 26,  211 => 25,  207 => 24,  203 => 23,  196 => 18,  187 => 17,  170 => 7,  152 => 6,  124 => 60,  120 => 59,  116 => 58,  112 => 57,  108 => 56,  103 => 55,  101 => 54,  86 => 41,  84 => 40,  77 => 35,  75 => 17,  68 => 13,  64 => 12,  60 => 11,  56 => 10,  52 => 9,  47 => 8,  45 => 7,  41 => 6,  34 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=\"UTF-8\" />
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+        <title>{% block title %}Hotel App{% endblock %}</title>
+        {% block stylesheets %}{% endblock %}
+        <link rel=\"icon\" type=\"image/x-icon\" href=\"{{ asset('favicon.ico') }}\" />
+        <link rel=\"stylesheet\" href=\"{{ asset('css/foundation.css') }}\">
+        <link rel=\"stylesheet\" href=\"{{ asset('css/app.css') }}\">
+        <link rel=\"stylesheet\" href=\"{{ asset('css/sidenav.css') }}\">
+        <link rel=\"stylesheet\" href=\"{{ asset('pickadate/lib/themes/default.css') }}\">
+        <link rel=\"stylesheet\" href=\"{{ asset('pickadate/lib/themes/default.date.css') }}\">
+
+    </head>
+    <body>
+    {% block top_bar %}
+        <!-- Start Top Bar -->
+    <div class=\"top-bar\">
+      <div class=\"row\">
+        <div class=\"top-bar-left\">
+          <ul class=\"dropdown menu\" data-dropdown-menu>
+            <li><a href=\"{{ path(\"home\")}}\">Home</a></li>
+              <li><a href=\"{{ path(\"about_us\")}}\">About us</a></li>
+              <li><a href=\"{{ path(\"location\")}}\">How to locate</a></li>
+              <li><a href=\"{{ path(\"contact\")}}\">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- End Top Bar -->
+
+
+    {% endblock %}
+    <noscript><h4 align=\"center\">For full functionality of this page it is necessary to enable JavaScript.
+            Here are the <a href=\"http://www.enable-javascript.com\" target=\"_blank\">
+                instructions how to enable JavaScript in your web browser</a></h4></noscript>
+    <br>
+
+        {% block body %} {% endblock %}
+    
+
+    <div class=\"row column\">
+      <hr>
+      <ul class=\"menu\">
+        <li class=\"float-right\">Copyright 2017</li>
+      </ul>
+    </div>
+
+    <script>
+      \$(document).foundation();
+    </script>
+
+        {% block javascripts %}{% endblock %}
+        <script src=\"{{ asset('js/vendor/jquery.js') }}\"></script>
+        <script src=\"{{ asset('js/vendor/what-input.js') }}\"></script>
+        <script src=\"{{ asset('js/vendor/foundation.js') }}\"></script>
+        <script src=\"{{ asset('js/app.js') }}\"></script>
+        <script src=\"{{ asset('pickadate/lib/picker.js') }}\"></script>
+        <script src=\"{{ asset('pickadate/lib/picker.date.js') }}\"></script>
+        <script src='https://www.google.com/recaptcha/api.js'></script>
+    <script>
+        \$('.datepicker').pickadate(
+            {
+                //format: 'yyyy-mm-dd',
+                //formatSubmit: 'yyyy-mm-dd',
+                format: 'd mmmm, yyyy',
+                min: new Date('today'),
+                max: new Date(2019,12,31),
+                today: 'Today'
+            }
+        );
+
+    </script>
+    </body>
+</html>
+", "base.html.twig", "/var/www/10.Edu_Symfony3_hotel/app/Resources/views/base.html.twig");
+    }
+}
